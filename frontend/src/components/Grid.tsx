5a9c8a5d44cb66a5484d3c341170257c49d20528
Josh Clark <jclarktennis@gmail.com>
2024-05-15 21:43:14 -0400

    feat: show actor names next to grid

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -2,24 +2,40 @@ import React from 'react';
 import Square from './Square';
 import { Grid as GridData } from '../../../common/interfaces';
 
-const Grid: React.FC<GridData> = poopyGridData => {
+const Grid: React.FC<GridData> = gridData => {
   console.log("Grid data:");
-  console.log(poopyGridData);
-  const size = 3;
+  console.log(gridData);
+  const size = 4;
   const squares = [];
+
+  if (!gridData || !gridData.actors || gridData.actors.length === 0) {
+    console.log("No actors in grid data");
+    return <div></div>;
+  }
 
   for (let rowIndex = 0; rowIndex < size; rowIndex++) {
     for (let columnIndex = 0; columnIndex < size; columnIndex++) {
+      const isAxisSquare = rowIndex === 0 || columnIndex === 0;
+      let squareText = "";
+      if (isAxisSquare) {
+        if (rowIndex === 0 && columnIndex === 0) {
+        } else if (rowIndex === 0) {
+          squareText = gridData.actors[columnIndex - 1].name;
+        } else {
+          squareText = gridData.actors[(size - 1) + (rowIndex - 1)].name;
+        }
+      }
       squares.push(
-        <div className={`border solid w-18 h-18 hover:bg-sky-100`} key={`${rowIndex}-${columnIndex}`}>
-          <Square initialRow={rowIndex} initialColumn={columnIndex} text={""} />
+        <div className={`${isAxisSquare ? "" : "border solid hover:bg-sky-100"} w-18 h-18`} key={`${rowIndex}-${columnIndex}`}>
+          <Square initialRow={rowIndex} initialColumn={columnIndex} text={squareText} />
+
         </div>
       );
     }
   }
 
   return (
-    <div className="grid grid-cols-3 gap-0 w-auto m-0 p-0" style={{ width: "30rem", height: "30rem" }}>
+    <div className="grid grid-cols-4 gap-0 w-auto m-0 p-0" style={{ width: "30rem", height: "30rem" }}>
       {squares}
     </div>
   );
