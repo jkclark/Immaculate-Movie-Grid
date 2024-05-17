7abd173b3cfe8e975a6db6748889a78bacbc7cf0
Josh Clark <jclarktennis@gmail.com>
2024-05-17 18:20:43 -0400

    feat: enable CORS

--- api/src/getImage.ts
+++ api/src/getImage.ts
@@ -29,7 +29,12 @@ export const handler: Handler = async (event: APIGatewayProxyEvent, context: Con
     return {
         statusCode: 200,
         headers: {
-            'Content-Type': `image/${imageType}`
+            "Content-Type": `image/${imageType}`,
+            // These headers are necessary when I want to hit this endpoint from localhost while I'm developing.
+            // I'm not sure if there's a better way to do this, but for now it's fine.
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Acess-Control-Allow-Methods": "GET"
         },
         body: imageBase64,
         isBase64Encoded: true,
