eb2763abc22067b2f4f879a70c730cffe31f96d4
Josh Clark <jclarktennis@gmail.com>
2024-05-19 12:04:02 -0400

    feat: do not show previous search results if query changed

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -4,14 +4,19 @@ import { SearchResult as SearchResultData } from "../../../common/src/interfaces
 import SearchResult from "./SearchResult";
 
 const SearchBar: React.FC = () => {
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
+  const [previousInputText, setPreviousInputText] = useState("");
   const [searchResults, setSearchResults] = useState<SearchResultData[]>([]);
 
   const fetchResults = useCallback(debounce((query) => {
     if (query.length > 0) {
       fetch(`https://api.immaculatemoviegrid.com/dev/search?query=${encodeURIComponent(query)}`)
         .then(response => response.json())
-        .then(data => { console.log("RESULTS:" + data.searchResults); setSearchResults(data.searchResults) })
+        .then(data => {
+          console.log("RESULTS:" + data.searchResults);
+          setSearchResults(data.searchResults);
+          setPreviousInputText(query);
+        })
         .catch(error => console.error(error));
     } else {
       setSearchResults([]);
@@ -31,11 +36,13 @@ const SearchBar: React.FC = () => {
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
           </svg>
         </div>
-        <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search any movie or TV show" required onChange={e => setInputText(e.target.value)} />
         <div className="relative">
-          {inputText && searchResults && searchResults.map((result, index) => (
-            <SearchResult key={index} {...result} />
-          ))}
+          <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search any movie or TV show" required onChange={e => setInputText(e.target.value)} />
+          <div className="absolute w-full">
+            {inputText && inputText === previousInputText && searchResults && searchResults.map((result, index) => (
+              <SearchResult key={index} {...result} />
+            ))}
+          </div>
         </div>
       </div>
     </form>
