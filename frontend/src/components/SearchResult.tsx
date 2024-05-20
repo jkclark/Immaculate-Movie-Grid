e79571c84c71a3fe0b9f6107aaee1a0701a1c5a0
Josh Clark <jclarktennis@gmail.com>
2024-05-19 20:20:31 -0400

    feat: add nonfunctional button to search results

--- frontend/src/components/SearchResult.tsx
+++ frontend/src/components/SearchResult.tsx
@@ -6,8 +6,11 @@ interface SearchResultProps extends SearchResultData {
 
 const SearchResult: React.FC<SearchResultProps> = ({ media_type, title, isLast }) => {
   return (
-    <div className={`bg-gray-800 text-white p-4 shadow-lg border border-gray-600 text-sm hover:bg-gray-700 ${isLast ? 'rounded-b-md' : ''}`}>
-      {`${media_type}: ${title}`}
+    <div className={`bg-gray-800 text-white p-4 shadow-lg border border-gray-600 text-sm hover:bg-gray-700 flex justify-between items-center ${isLast ? 'rounded-b-md' : ''}`}>
+      <span>{`${media_type}: ${title}`}</span>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Select
+      </button>
     </div>
   );
 }
