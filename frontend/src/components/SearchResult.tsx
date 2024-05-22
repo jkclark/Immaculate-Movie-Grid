5fa7aea95a76adf97708fd41d1d457230817f40b
Josh Clark <jclarktennis@gmail.com>
2024-05-22 12:51:35 -0400

    feat: hide button and make text red on wrong guess

--- frontend/src/components/SearchResult.tsx
+++ frontend/src/components/SearchResult.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { SearchResult as SearchResultData } from "../../../common/src/interfaces";
 
 interface SearchResultProps extends SearchResultData {
@@ -6,17 +7,21 @@ interface SearchResultProps extends SearchResultData {
 }
 
 const SearchResult: React.FC<SearchResultProps> = ({ media_type, id, title, checkAnswerFunc, setTextAndImageFunc }) => {
+  const [isWrong, setIsWrong] = useState(false);
+
   function handleClick(event: React.MouseEvent) {
     event.preventDefault();  // For whatever reason the button refreshes the page without this
     if (checkAnswerFunc(media_type, id)) {
       setTextAndImageFunc(media_type, id, title);
+    } else {
+      setIsWrong(true);
     }
   }
 
   return (
-    <div className={"bg-gray-800 text-white p-4 shadow-lg border border-gray-600 text-sm hover:bg-gray-700 flex justify-between items-center"}>
-      <span>{`${media_type === "movie" ? "📽️" : "📺"} ${title}`}</span>
-      <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <div className={"bg-gray-800 p-4 shadow-lg border border-gray-600 text-sm hover:bg-gray-700 flex justify-between items-center"}>
+      <span className={`${isWrong ? "text-red-700" : "text-white"}`}>{`${media_type === "movie" ? "📽️" : "📺"} ${title}`}</span>
+      <button onClick={handleClick} className={`${isWrong ? "invisible" : ""} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}>
         Select
       </button>
     </div>
