300102445e98cb4321fa7ad365149943510bcb26
Josh Clark <jclarktennis@gmail.com>
2024-05-25 18:50:24 -0400

    fix: do not exclude any credits when fetching graph data

--- grid_generation/src/tmdbAPI.ts
+++ grid_generation/src/tmdbAPI.ts
@@ -34,52 +34,11 @@ export async function getActorCredits(actor: Actor): Promise<Set<Credit>> {
 
     const credits: Set<Credit> = new Set();
     for (const credit of responseJson["cast"]) {
-        if (!isCreditValid(credit)) {
-            continue;
-        }
-
         // Movies have a "title", TV shows have a "name"
         credits.add({ type: credit.media_type, id: credit.id, name: credit.title || credit.name });
     }
 
     return credits;
-}
-
-/**
- * Determine if a credit is "valid". A credit is valid if it is:
- * - Not a TV show in an "invalid" genre
- * - Not an "invalid" TV show
- * 
- * I'm using `any` instead of a more specific type because the TMDB API response
- * is large and complex.
- * 
- * Notes:
- * - There are some TV shows, like CSI: Crime Scene Investigation and NCIS that have
- *   had so many episodes with so many guest actors that they are starting to move away
- *   from the spirit of the game. Not sure yet what to do about that.
- * 
- * @param credit the JSON object from the TMDB API representing a credit
- * @returns true if the credit is valid, false otherwise
- */
-function isCreditValid(credit: any): boolean {
-    const INVALID_TV_GENRES_IDS: number[] = [
-        10763, // News
-        10767, // Talk shows
-    ]
-    const isInvalidGenre: boolean = credit.media_type === "tv" && credit.genre_ids.some(id => INVALID_TV_GENRES_IDS.includes(id));
-
-    const INVALID_TV_SHOW_IDS: number[] = [
-        1667, // Saturday Night Live
-        2224, // The Daily Show
-        3739, // E! True Hollywood Story
-        13667, // MTV Movie & TV Awards
-        27023, // The Oscars
-        30048, // Tony Awards
-        89293, // Bambi Awards
-        1111889, // Carol Burnett: 90 Years of Laughter + Love
-    ]
-    const isInvalidShow: boolean = credit.media_type === "tv" && INVALID_TV_SHOW_IDS.includes(credit.id);
-    return !isInvalidGenre && !isInvalidShow;
 }
 
 export async function getImageByIdTypeAndSize(imagesBaseURL: string, id: number, type: "actor" | "tv" | "movie", size: string): Promise<[Readable, string]> {
