3e53d78c503d520c4a0329633d826798b6377b7f
Josh Clark <jclarktennis@gmail.com>
2024-06-05 23:42:08 -0400

    fix: stop clicks on CorrectCreditsSummary from closing overlay

--- frontend/src/components/CorrectCreditsSummary.tsx
+++ frontend/src/components/CorrectCreditsSummary.tsx
@@ -10,7 +10,10 @@ const CorrectCreditsSummary: React.FC<CorrectCreditsSummaryProps> = ({ credits }
   const tvShows = credits.filter((credit) => credit.type === "tv");
 
   return (
-    <div className="flex flex-col md:flex-row bg-white dark:bg-gray-800 w-4/5 md:w-2/3 max-w-[800px] h-1/2 rounded-lg shadow-lg p-2 md:p-6">
+    <div
+      onClick={(e) => e.stopPropagation()}
+      className="flex flex-col md:flex-row bg-white dark:bg-gray-800 w-4/5 md:w-2/3 max-w-[800px] h-1/2 rounded-lg shadow-lg p-2 md:p-6"
+    >
       <div className="flex flex-col w-full md:w-1/2 text-center">
         <h2 className="text-xl font-bold mb-4">Movies</h2>
         <ul className="mb-8 flex flex-col">
