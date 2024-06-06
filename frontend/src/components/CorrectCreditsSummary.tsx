c2244056b7f9d57fe224ea58bd5c8efbd5e5cec6
Josh Clark <jclarktennis@gmail.com>
2024-06-06 18:20:53 -0400

    feat: make CorrectCreditsSummary more stylish

--- frontend/src/components/CorrectCreditsSummary.tsx
+++ frontend/src/components/CorrectCreditsSummary.tsx
@@ -14,29 +14,44 @@ const CorrectCreditsSummary: React.FC<CorrectCreditsSummaryProps> = ({ credits }
       onClick={(e) => e.stopPropagation()}
       className="flex flex-col md:flex-row bg-white dark:bg-gray-800 w-4/5 md:w-2/3 max-w-[800px] h-1/2 rounded-lg shadow-lg p-2 md:p-6"
     >
-      <div className="flex flex-col w-full md:w-1/2 text-center">
-        <h2 className="text-xl font-bold mb-4">Movies</h2>
-        <ul className="mb-8 flex flex-col">
-          {movies.map((movie) => (
-            <li key={movie.id} className="text-gray-700 dark:text-gray-300">
-              {movie.name}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="flex flex-col w-full md:w-1/2 text-center">
-        <h2 className="text-xl font-bold mb-4">TV Shows</h2>
-        <ul className="flex flex-col">
-          {tvShows.map((show) => (
-            <li key={show.id} className="text-gray-700 dark:text-gray-300">
-              {show.name}
-            </li>
-          ))}
-        </ul>
-      </div>
+      {CreditList("Movies", movies)}
+      {CreditList("TV Shows", tvShows)}
     </div>
   );
 };
+
+function CreditList(title: string, credits: CreditExport[]) {
+  return (
+    <div className="flex flex-col w-full md:w-1/2 mx-3 text-center overflow-auto max-h-[desiredHeight]">
+      <h2 className="text-xl font-bold mb-4">{title}</h2>
+      <ul className="mb-8 flex flex-col">
+        {credits.map((credit) => (
+          <li
+            key={credit.id}
+            className="text-gray-700 dark:text-gray-300 flex items-center justify-between cursor-pointer mb-2"
+          >
+            <div className="flex-1 overflow-hidden">
+              <p className="truncate whitespace-nowrap">{credit.name}</p>
+            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="h-6 w-6 ml-2 hover:text-blue-500"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <line x1="12" y1="16" x2="12" y2="12" />
+              <line x1="12" y1="8" x2="12" y2="8" />
+            </svg>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default CorrectCreditsSummary;
