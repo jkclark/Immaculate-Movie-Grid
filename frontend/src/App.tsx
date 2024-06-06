6937b10ed2cb96fc26b2343df21bb0175dd4d4d9
Josh Clark <jclarktennis@gmail.com>
2024-06-05 23:13:36 -0400

    refactor: move postgame logic to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { CreditExport } from "../../common/src/interfaces";
 import Grid from "./components/Grid";
 import Overlay, { overlayContentsAtom } from "./components/Overlay";
 import SearchBar from "./components/SearchBar";
 import {
   AnyGridDisplayData,
+  TextGridDisplayData,
   getGuessesRemainingGridDatum,
   getInitialGridDisplayData,
   gridDisplayDataAtom,
   insertGridDisplayDatumAtRowCol,
   insertInnerGridDisplayData,
 } from "./gridDisplayData";
-import { getAllAnswerGridDisplayData } from "./logic/PostGameLogic";
 import { getGridDataFromS3, getS3ImageURLForType, preloadImageURL } from "./s3";
 import {
   finalGameGridDisplayDataAtom,
@@ -127,6 +128,71 @@ function App() {
     setSelectedCol(-1);
   }
 
+  function getAllAnswerGridDisplayData(): AnyGridDisplayData[][] {
+    const newInnerGridData: TextGridDisplayData[][] = [];
+    const acrossActors = gridData.actors.slice(0, gridData.actors.length / 2);
+    const downActors = gridData.actors.slice(gridData.actors.length / 2);
+    for (const downActor of downActors) {
+      const innerGridRow: TextGridDisplayData[] = [];
+      for (const acrossActor of acrossActors) {
+        const answers = getAnswersForPair(acrossActor.id, downActor.id);
+        const answerText = `${answers.length}`;
+        innerGridRow.push({
+          mainText: answerText,
+          clickHandler: () => {
+            console.log(`Clicked on ${acrossActor.name} and ${downActor.name}`);
+          },
+        });
+      }
+      newInnerGridData.push(innerGridRow);
+    }
+
+    // Get grid data with axes populated
+    const newGridData: AnyGridDisplayData[][] = getInitialGridDisplayData(gridData);
+
+    // Replace "guesses left" with total number of answers
+    const newGridDataWithTotal = insertGridDisplayDatumAtRowCol(
+      {
+        mainText: `${gridData.credits.length}`,
+        subText: "total",
+      },
+      0,
+      0,
+      newGridData
+    );
+    return insertInnerGridDisplayData(newGridDataWithTotal, newInnerGridData);
+  }
+
+  function getAnswersForPair(actor1Id: number, actor2Id: number): CreditExport[] {
+    const usedTypesAndIds = new Set<string>();
+    const answers: CreditExport[] = [];
+    const actor1Answers = gridData.answers[actor1Id];
+    const actor2Answers = gridData.answers[actor2Id];
+
+    for (const actor1Answer of actor1Answers) {
+      for (const actor2Answer of actor2Answers) {
+        if (actor1Answer.type === actor2Answer.type && actor1Answer.id === actor2Answer.id) {
+          // Skip if we've already added this credit to the answers
+          if (usedTypesAndIds.has(`${actor1Answer.type}-${actor1Answer.id}`)) {
+            continue;
+          }
+
+          // Look up this credit's title in the credits array
+          const answer = gridData.credits.find((credit) => credit.id === actor1Answer.id);
+          if (answer) {
+            // Add to used set
+            usedTypesAndIds.add(`${actor1Answer.type}-${actor1Answer.id}`);
+
+            // Add to answers
+            answers.push(answer);
+          }
+        }
+      }
+    }
+
+    return answers;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen dark:bg-gray-800 dark:text-white relative">
       {gameOver && (
@@ -143,7 +209,7 @@ function App() {
           <button
             onClick={() => {
               setActiveTab("All answers");
-              setGridDisplayData(getAllAnswerGridDisplayData(gridData));
+              setGridDisplayData(getAllAnswerGridDisplayData());
             }}
             className={`${activeTab === "All answers" ? "bg-blue-700" : ""}`}
           >
