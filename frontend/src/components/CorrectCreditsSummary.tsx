84103196d15bdb61759a7d254584532bba09c1c0
Josh Clark <jclarktennis@gmail.com>
2024-06-05 23:28:12 -0400

    feat: make 'correct answers' view more responsive

--- frontend/src/components/CorrectCreditsSummary.tsx
+++ frontend/src/components/CorrectCreditsSummary.tsx
@@ -10,8 +10,8 @@ const CorrectCreditsSummary: React.FC<CorrectCreditsSummaryProps> = ({ credits }
   const tvShows = credits.filter((credit) => credit.type === "tv");
 
   return (
-    <div className="flex bg-white dark:bg-gray-800 w-2/3 h-1/2 rounded-lg shadow-lg p-6">
-      <div className="flex flex-col w-1/2 text-center">
+    <div className="flex flex-col md:flex-row bg-white dark:bg-gray-800 w-4/5 md:w-2/3 max-w-[800px] h-1/2 rounded-lg shadow-lg p-2 md:p-6">
+      <div className="flex flex-col w-full md:w-1/2 text-center">
         <h2 className="text-xl font-bold mb-4">Movies</h2>
         <ul className="mb-8 flex flex-col">
           {movies.map((movie) => (
@@ -22,7 +22,7 @@ const CorrectCreditsSummary: React.FC<CorrectCreditsSummaryProps> = ({ credits }
         </ul>
       </div>
 
-      <div className="flex flex-col w-1/2 text-center">
+      <div className="flex flex-col w-full md:w-1/2 text-center">
         <h2 className="text-xl font-bold mb-4">TV Shows</h2>
         <ul className="flex flex-col">
           {tvShows.map((show) => (
