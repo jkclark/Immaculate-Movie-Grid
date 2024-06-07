abd35da6534f1437daca3232376aed621b69a5b8
Josh Clark <jclarktennis@gmail.com>
2024-06-07 17:00:15 -0400

    chore: adjust padding

--- frontend/src/components/AboutDisplay.tsx
+++ frontend/src/components/AboutDisplay.tsx
@@ -23,12 +23,13 @@ const AboutDisplay: React.FC = () => {
           <h1 className="text-3xl font-bold">Credits</h1>
           <br />
           <p className="text-base">
-            All of the data for this game comes from the amazing{" "}
+            All of the data used in this game comes from the amazing{" "}
             <a href="https://www.themoviedb.org/" className="text-blue-500 underline">
               TMDB
             </a>
             , who generously provide a free-to-use API.
           </p>
+          <br />
           <p>
             The game also uses{" "}
             <a href="https://www.flaticon.com/packs/cinema-183" className="text-blue-500 underline">
--- frontend/src/components/SimpleTextDisplay.tsx
+++ frontend/src/components/SimpleTextDisplay.tsx
@@ -8,7 +8,7 @@ const SimpleTextDisplay: React.FC<SimpleTextDisplayProps> = ({ content }) => {
   return (
     <div
       onClick={(e) => e.stopPropagation()}
-      className="flex flex-col md:flex-row bg-white dark:bg-gray-800 w-4/5 md:w-2/3 max-w-[800px] h-1/2 rounded-lg shadow-lg p-3 md:p-6 relative overflow-auto"
+      className="flex flex-col md:flex-row bg-white dark:bg-gray-800 w-4/5 md:w-2/3 max-w-[800px] h-1/2 rounded-lg shadow-lg py-3 px-4 md:p-6 relative overflow-auto"
     >
       {content}
     </div>
