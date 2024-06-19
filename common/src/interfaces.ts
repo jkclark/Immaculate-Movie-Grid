bed20c1e1aa12b41590d3c106aba5621639bc650
Josh Clark <jclarktennis@gmail.com>
2024-06-18 22:50:42 -0400

    chore: set file tab display from 4 spaces -> 2 spaces

--- common/src/interfaces.ts
+++ common/src/interfaces.ts
@@ -1,25 +1,26 @@
 export interface ActorExport {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
-export interface CreditExport {  // For now this is the same as Credit
-    type: "movie" | "tv";
-    id: number;
-    name: string;
+export interface CreditExport {
+  // For now this is the same as Credit
+  type: "movie" | "tv";
+  id: number;
+  name: string;
 }
 
 export interface Grid {
-    actors: ActorExport[];
-    credits: CreditExport[];
-    answers: { [key: number]: { type: "movie" | "tv", id: number }[] } // actor id -> [{type, id}, ...]
+  actors: ActorExport[];
+  credits: CreditExport[];
+  answers: { [key: number]: { type: "movie" | "tv"; id: number }[] }; // actor id -> [{type, id}, ...]
 }
 
 export interface SearchResult {
-    media_type: "movie" | "tv";
-    id: number;
-    title: string;
-    release_date?: string;   // Only for movies
-    first_air_date?: string; // Only for TV shows
-    last_air_date?: string;  // Only for TV shows
+  media_type: "movie" | "tv";
+  id: number;
+  title: string;
+  release_date?: string; // Only for movies
+  first_air_date?: string; // Only for TV shows
+  last_air_date?: string; // Only for TV shows
 }
