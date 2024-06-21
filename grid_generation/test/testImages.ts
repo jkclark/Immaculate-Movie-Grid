b9ad5c3c97bbee2e886d073d5ff06dec8ad3ac6a
Josh Clark <jclarktennis@gmail.com>
2024-06-20 22:48:47 -0400

    fix: update test file after prolonged disuse

--- grid_generation/test/testImages.ts
+++ grid_generation/test/testImages.ts
@@ -1,18 +1,16 @@
-import { Grid } from "../../common/src/interfaces";
+import { GridExport } from "../../common/src/interfaces";
 import { getAndSaveAllImagesForGrid } from "../src/images";
 
 async function main(): Promise<void> {
-    const grid: Grid = {
-        actors: [
-            { id: 16483, name: "Sylvester Stallone" },
-        ],
-        credits: [
-            { type: "movie", id: 238, name: "The Godfather" },
-            { type: "tv", id: 1668, name: "Friends" }
-        ],
-        answers: [],
-    }
-    await getAndSaveAllImagesForGrid(grid);
+  const grid: GridExport = {
+    actors: [{ id: 16483, name: "Sylvester Stallone" }],
+    credits: [
+      { type: "movie", id: 238, name: "The Godfather" },
+      { type: "tv", id: 1668, name: "Friends" },
+    ],
+    answers: [],
+  };
+  await getAndSaveAllImagesForGrid(grid, false);
 }
 
 main();
