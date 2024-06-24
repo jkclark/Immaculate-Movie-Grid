6773880c00b8e214c46622fd0acb6902d185d60b
Josh Clark <jclarktennis@gmail.com>
2024-06-23 22:41:47 -0400

    chore: remove unused grid-pagination code

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -7,31 +7,13 @@ interface GridProps {
 }
 
 const Grid: React.FC<GridProps> = ({ gridDisplayData }) => {
-  // 4 x 4 grid
-  const numPages = gridDisplayData.length / 4 ** 2;
-  const [currentPage, setCurrentPage] = React.useState(0);
-  const gridDisplayDataPage = gridDisplayData.slice(currentPage * 4 ** 2, (currentPage + 1) * 4 ** 2);
   return (
     <div>
-      {numPages > 1 && (
-        <div className="flex justify-between">
-          <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0}>
-            Previous
-          </button>
-          <button
-            onClick={() => setCurrentPage(currentPage + 1)}
-            disabled={currentPage >= gridDisplayData.length - 1}
-          >
-            Next
-          </button>
-        </div>
-      )}
       <div
         className="grid grid-cols-4 grid-rows-4 max-w-[60vh] px-4"
         style={{ marginTop: "calc(2vh + 20px)" }}
       >
-        {/* As a 2D array, this does not render properly */}
-        {gridDisplayDataPage.flat().map((square, index) => (
+        {gridDisplayData.flat().map((square, index) => (
           <GridSquare key={index} {...square} row={Math.floor(index / 4)} col={index % 4} />
         ))}
       </div>
