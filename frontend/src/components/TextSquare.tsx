cc497b3a99a4d8aec22f00019bd54cea24e62144
Josh Clark <jclarktennis@gmail.com>
2024-08-25 11:52:12 -0400

    fix: render grid text at appropriate size for the screen

--- frontend/src/components/TextSquare.tsx
+++ frontend/src/components/TextSquare.tsx
@@ -12,20 +12,55 @@ const TextSquare: React.FC<TextSquareProps> = ({ mainText, subText, clickHandler
     }
   };
 
-  const mainTextLength = mainText.length;
-  const mainTextClassSize = 6 - Math.floor(mainTextLength / 3);
-  let mainTextClass = `text-${mainTextClassSize}xl`;
-  // The smallest font size utility with a number is text-2xl
-  if (mainTextClassSize <= 1) {
-    mainTextClass = "text-xl";
+  const smallScreenTextClasses = [
+    "text-xs",
+    "text-sm",
+    "text-base",
+    "text-lg",
+    "text-xl",
+    "text-2xl",
+    "text-3xl",
+  ];
+
+  const largeScreenTextClasses = [
+    "text-xl",
+    "text-2xl",
+    "text-3xl",
+    "text-4xl",
+    "text-5xl",
+    "text-6xl",
+    "text-7xl",
+  ];
+
+  function getTextClassForString(text: string, textClasses: string[], baseIndex: number): string {
+    const textLength = text.length;
+    // This is just the (very basic) forula we're using for now to determine the offset
+    const offset = Math.floor(textLength / 2);
+    const textClassIndex = Math.max(baseIndex - offset, 0);
+    return textClasses[textClassIndex];
   }
+
+  const smallScreenMainTextClass = getTextClassForString(
+    mainText,
+    smallScreenTextClasses,
+    smallScreenTextClasses.length - 1
+  );
+
+  const largeScreenMainTextClass = getTextClassForString(
+    mainText,
+    largeScreenTextClasses,
+    largeScreenTextClasses.length - 1
+  );
+
+  let responsiveMainTextClass = `${smallScreenMainTextClass} sm:${largeScreenMainTextClass}`;
+  console.log(`Setting ${mainText} to ${responsiveMainTextClass}`);
 
   return (
     <div
       onClick={handleClick}
       className="w-full h-full cursor-default text-center flex flex-col item-center justify-center"
     >
-      <p className={`${mainTextClass} hover:cursor-default`}>{mainText}</p>
+      <p className={`${responsiveMainTextClass} hover:cursor-default`}>{mainText}</p>
       {subText && <p className="text-xl">{subText}</p>}
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["index.html", "src/**/*.{vue,js,ts,jsx,tsx}"],
-  safelist: ["text-xl", "text-2xl", "text-3xl", "text-4xl", "text-5xl", "text-6xl", "text-7xl"],
+  safelist: [
+    "text-xl",
+    "text-2xl",
+    "text-3xl",
+    "text-4xl",
+    "text-5xl",
+    "text-6xl",
+    "text-7xl",
+    "sm:text-xl",
+    "sm:text-2xl",
+    "sm:text-3xl",
+    "sm:text-4xl",
+    "sm:text-5xl",
+    "sm:text-6xl",
+    "sm:text-7xl",
+  ],
   theme: {
     extend: {
       spacing: {
