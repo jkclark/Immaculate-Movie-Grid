2a3ada6d9417a47590aec1c1453cf53d5c3d80d6
Josh Clark <jclarktennis@gmail.com>
2024-09-07 09:17:49 -0400

    chore: update actor-category ratio

--- grid_generation/src/index.ts
+++ grid_generation/src/index.ts
@@ -75,7 +75,7 @@ async function main(): Promise<void> {
   let grid: Grid;
   do {
     // Get a valid grid from the generic graph
-    grid = getGridFromGraph(genericGraph, 3, { actor: 0.9, category: 0.1 }, true);
+    grid = getGridFromGraph(genericGraph, 3, { actor: 0.95, category: 0.05 }, true);
 
     // If no valid grid was found, exit
     if (grid.across.length === 0 || grid.down.length === 0) {
