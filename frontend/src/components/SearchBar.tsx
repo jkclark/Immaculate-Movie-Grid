a3cdf444f4ec862709f952ccb12f13b268fca015
Josh Clark <jclarktennis@gmail.com>
2024-09-18 10:30:46 -0400

    fix: up font size to disable iOS auto zoom
    On iOS, (it seems like) if the font size for an input field is less than
    16px, the screen will get zoomed in automatically. This is annoying,
    because once the user is done typing, the zoom doesn't undo itself. This
    PR ups the font size to 16px so this doesn't happen.

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -86,7 +86,7 @@ const SearchBar: React.FC = () => {
             ref={inputRef}
             type="search"
             id="default-search"
-            className={`block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+            className={`block w-full p-4 ps-10 text-base text-gray-900 border border-gray-300 rounded bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
             placeholder="Search any movie or TV show"
             required
             onChange={(e) => setInputText(e.target.value)}
