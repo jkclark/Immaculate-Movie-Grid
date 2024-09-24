693c4a2cfd4a1cea6b5c9804e1b8cbf93f0a82e8
Josh Clark <jclarktennis@gmail.com>
2024-09-24 12:20:55 -0400

    feat: add basic DB connectivity

--- grid_generation/src/dbGraph.ts
+++ grid_generation/src/dbGraph.ts
@@ -1,8 +1,21 @@
+import { AppDataSource, initializeDataSource } from "common/src/db/connect";
+import { ActorOrCategory } from "common/src/db/models/ActorOrCategory";
 import { ActorCreditGraph } from "./interfaces";
 
 export async function loadGraphFromDB(): Promise<ActorCreditGraph> {
+  await initializeDataSource();
+
+  const actorRepository = AppDataSource.getRepository(ActorOrCategory);
+  const actor = await actorRepository.findOne({ where: { name: "josh" } });
+
+  console.log(actor);
+
   return {
     actors: {},
     credits: {},
   };
+}
+
+if (require.main === module) {
+  loadGraphFromDB();
 }
