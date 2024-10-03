52ec029b4ec4e59a1a946f42d07befd712d073d0
Josh Clark <jclarktennis@gmail.com>
2024-10-03 11:20:38 -0400

    fix: fix all issues in user data script

--- grid_generation/fetch_save_all_data_user_data.sh
+++ grid_generation/fetch_save_all_data_user_data.sh
@@ -1,27 +1,67 @@
 #!/bin/bash
-apt-get update
+### Move to home directory
+cd ~
 
-### Clone the GitHub repository
-git clone https://github.com/jkclark/Immaculate-Movie-Grid.git
+### Install necessary packages
+sudo dnf update -y
+sudo dnf install -y jq git npm amazon-cloudwatch-agent
+
+### Configure and start CloudWatch Logs agent
+
+# Create CloudWatch Logs configuration file
+sudo bash -c 'cat <<EOL > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+{
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/var/log/syslog",
+            "log_group_name": "/ec2-instance/syslog",
+            "log_stream_name": "{instance_id}",
+            "timestamp_format": "%b %d %H:%M:%S"
+          },
+          {
+            "file_path": "/var/log/cloud-init.log",
+            "log_group_name": "/ec2-instance/cloud-init",
+            "log_stream_name": "{instance_id}",
+            "timestamp_format": "%Y-%m-%d %H:%M:%S"
+          },
+          {
+            "file_path": "/var/log/cloud-init-output.log",
+            "log_group_name": "/ec2-instance/cloud-init-output",
+            "log_stream_name": "{instance_id}",
+            "timestamp_format": "%Y-%m-%d %H:%M:%S"
+          }
+        ]
+      }
+    }
+  }
+}
+EOL'
+
+# Start the CloudWatch Logs agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
 ### Set up environment variables
-# Install jq to parse JSON
-apt-get install -y jq
-
 # Get the TMDB API key from Secrets Manager
-export TMDB_API_KEY=$(aws secretsmanager get-secret-value --secret-id "dev/tmdb-api-key" --query "SecretString" --output text | jq -r '.')
+export TMDB_API_KEY=$(aws secretsmanager get-secret-value --secret-id "dev/tmdb-api-key" --query "SecretString" --output text | jq -r '.TMDB_API_KEY')
 
 # Get the RDS information from Secrets Manager
-RDS_INFO=$(aws secretsmanager get-secret-value --secret-id "dev/rds/postgres")
+RDS_INFO=$(aws secretsmanager get-secret-value --secret-id "dev/rds/postgres" --query "SecretString" --output text)
+RDS_INFO_PARSED=$(echo $RDS_INFO | jq -r '.')
 
 # Store the RDS information in environment variables
-export POSTGRES_HOST=$(echo $RDS_INFO | jq -r '.host')
-export POSTGRES_PORT=$(echo $RDS_INFO | jq -r '.port')
-export POSTGRES_USER=$(echo $RDS_INFO | jq -r '.username')
-export POSTGRES_PASSWORD=$(echo $RDS_INFO | jq -r '.password')
-export POSTGRES_DB=movie_grid
+export POSTGRES_HOST=$(echo $RDS_INFO_PARSED | jq -r '.host')
+export POSTGRES_PORT=$(echo $RDS_INFO_PARSED | jq -r '.port')
+export POSTGRES_USER=$(echo $RDS_INFO_PARSED | jq -r '.username')
+export POSTGRES_PASSWORD=$(echo $RDS_INFO_PARSED | jq -r '.password')
+export POSTGRES_DB=$(echo $RDS_INFO_PARSED | jq -r '.dbname')
 
-### Run the script
+### Download and run the script
+# Clone the repo
+git clone https://github.com/jkclark/Immaculate-Movie-Grid.git
+
 # Move to the project directory
 cd Immaculate-Movie-Grid/grid_generation
 
@@ -29,7 +69,11 @@ cd Immaculate-Movie-Grid/grid_generation
 npm install
 
 # Run the script
-npx ts-node ./src/fetchAndSaveAllData.ts
+npx ts-node ./src/fetchAndSaveAllData.ts db
 
-### Shutdown the instance
-aws ec2 terminate-instances --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+### Shutdown the instance using the metadata service V2
+# Get session token
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+
+# Shutdown the instance
+aws ec2 terminate-instances --instance-ids $(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id) --region us-east-1
