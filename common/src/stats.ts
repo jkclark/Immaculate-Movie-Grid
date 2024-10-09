5545971c391a717dbe5c1fbbbd4ea0bb759cb0bd
Josh Clark <jclarktennis@gmail.com>
2024-10-09 18:10:24 -0400

    feat: add first draft of 'writeGameStats' logic

--- common/src/stats.ts
+++ common/src/stats.ts
@@ -1,9 +1,15 @@
 import { AppDataSource, initializeDataSource } from "./db/connect";
-import { batchReadFromDB } from "./db/crud";
+import { batchReadFromDB, batchWriteToDB } from "./db/crud";
+import { Answer } from "./db/models/Answer";
 import { Score } from "./db/models/Score";
 
 interface Stats {
   numGames: number;
+}
+
+interface SingleGameAnswers {
+  gridDate: string;
+  answers: Answer[];
 }
 
 async function getStatsForGrid(gridDate: string): Promise<Stats> {
@@ -27,6 +33,30 @@ async function getAllScores(gridDate: string): Promise<Score[]> {
     grid: { date: gridDate },
   });
   return scores;
+}
+
+async function writeGameStats(answers: SingleGameAnswers): Promise<void> {
+  // Establish a connection to the database
+  await initializeDataSource();
+
+  // Calculate this game's score
+  let points = 0;
+  for (const answer of answers.answers) {
+    if (answer.correct) {
+      points++;
+    }
+  }
+
+  // Write answers to the database
+  const answerRepo = AppDataSource.getRepository(Answer);
+  await batchWriteToDB(answers.answers, answerRepo, 1000, []);
+
+  // Write score to the database
+  const scoreRepo = AppDataSource.getRepository(Score);
+  const score = new Score();
+  score.grid = answers.answers[0].grid;
+  score.score = points;
+  await scoreRepo.save(score);
 }
 
 if (require.main === module) {
