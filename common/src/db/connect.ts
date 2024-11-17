dd15a04894180f56942b2ae3b26dcbe470812bd8
Josh Clark <jclarktennis@gmail.com>
2024-11-16 22:33:12 -0500

    chore: add note about retrieving database creds

--- common/src/db/connect.ts
+++ common/src/db/connect.ts
@@ -22,7 +22,12 @@ const allEntities = [
   Answer,
 ];
 
-// TODO: Update the connection details
+// NOTE: We would really like to use the values from Secrets Manger to access the database.
+// However, I'm having issues related to having an await statement in the top level of the file.
+// Supposedly, this is allowed with different `module` values in the tsconfig.json file.
+// At this point, I'm too afraid to change that, given how much trouble it's shown to be in the past.
+// So, I'm just going to use environment variables for now. This means updating each Lambda function
+// that connects to the database whenever the database credentials change.
 const POSTGRES_USER = process.env.POSTGRES_USER;
 const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
 const POSTGRES_HOST = process.env.POSTGRES_HOST;
