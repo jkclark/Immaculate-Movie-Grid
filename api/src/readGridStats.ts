6467a21fcf6c78168c35ac9f689e84804ccdbc74
Josh Clark <jclarktennis@gmail.com>
2024-11-26 17:06:08 -0500

    fix: add CORS headers to stats Lambda functions

--- api/src/readGridStats.ts
+++ api/src/readGridStats.ts
@@ -10,6 +10,14 @@ export const handler: Handler = async (event: APIGatewayProxyEvent, context: Con
 
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      // These headers are necessary when I want to hit this endpoint from localhost while I'm developing.
+      // I'm not sure if there's a better way to do this, but for now it's fine.
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Acess-Control-Allow-Methods": "GET",
+    },
     body: JSON.stringify(stats),
   };
 };
--- api/src/writeGameStats.ts
+++ api/src/writeGameStats.ts
@@ -17,6 +17,14 @@ export const handler: Handler = async (event: APIGatewayProxyEvent, context: Con
 
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      // These headers are necessary when I want to hit this endpoint from localhost while I'm developing.
+      // I'm not sure if there's a better way to do this, but for now it's fine.
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Acess-Control-Allow-Methods": "POST",
+    },
     body: JSON.stringify("Stats saved"),
   };
 };
