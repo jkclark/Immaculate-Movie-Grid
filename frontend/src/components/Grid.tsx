f53fa0e2cdba17d31b00615e53c60545f0404fd5
Josh Clark <jclarktennis@gmail.com>
2024-12-15 20:50:05 -0500

    fix: make grid scale correctly with window size

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -10,7 +10,7 @@ const Grid: React.FC<GridProps> = ({ gridDisplayData }) => {
   return (
     <div>
       <div
-        className="grid grid-cols-4 grid-rows-4 max-w-[90vw] w-[60vh] aspect-[2/3] px-4"
+        className="grid grid-cols-4 grid-rows-4 max-w-[60vh] px-4"
         style={{ marginTop: "calc(2vh + 20px)" }}
       >
         {gridDisplayData.flat().map((square, index) => (
