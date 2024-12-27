7f2616fa553de299c0cef1b63470bd31d4d8e58f
Josh Clark <jclarktennis@gmail.com>
2024-12-27 18:16:40 -0500

    chore: remove unused 'style' prop for Grid

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -7,18 +7,14 @@ import GridSquare from "./GridSquare";
 interface GridProps {
   size: number;
   gridDisplayData: AnyGridDisplayData[][];
-  style?: React.CSSProperties;
 }
 
 // NOTE: If you ever want to pass in a new value to the Grid's size param,
 // you need to add the corresponding `grid-cols-${size}` and `grid-rows-${size}`
 // classes to tailwind.config.js in the safelist.
-const Grid: React.FC<GridProps> = ({ gridDisplayData, size, style }) => {
+const Grid: React.FC<GridProps> = ({ gridDisplayData, size }) => {
   return (
-    <div
-      className={`grid-container aspect-[2/3] grid grid-cols-${size} grid-rows-${size} mx-auto`}
-      style={style}
-    >
+    <div className={`grid-container aspect-[2/3] grid grid-cols-${size} grid-rows-${size} mx-auto`}>
       {gridDisplayData.flat().map((square, index) => (
         <GridSquare key={index} {...square} row={Math.floor(index / size)} col={index % size} />
       ))}
