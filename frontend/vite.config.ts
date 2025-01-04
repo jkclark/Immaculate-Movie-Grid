f1a8bd554009a74a21952215f62cdd18c1427384
Josh Clark <jclarktennis@gmail.com>
2025-01-04 17:06:10 -0500

    fix: specify path alias for common in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      common: path.resolve(__dirname, "../common/"),
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
