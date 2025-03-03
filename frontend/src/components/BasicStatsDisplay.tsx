59db5532878e2eba9b18156ff9f3bf24811edf58
Josh Clark <jclarktennis@gmail.com>
2025-03-02 23:14:40 -0500

    feat: add misc improvements to scores bar chart
    - put the chart in a card
    - theme colors
    - fix label issue for max-height bars
    - add minimum bar height

--- frontend/src/components/BasicStatsDisplay.tsx
+++ frontend/src/components/BasicStatsDisplay.tsx
@@ -1,9 +1,10 @@
 import { useAtomValue } from "jotai";
 import { useEffect, useState } from "react";
-import { Bar, BarChart, XAxis } from "recharts";
+import { Bar, BarChart, LabelList, XAxis, YAxis } from "recharts";
 import { gridIdAtom, gridStatsAtom, usedAnswersAtom } from "../state";
 import { roundToNearestNDigits } from "../utils";
 import SimpleTextDisplay from "./SimpleTextDisplay";
+import { Card, CardHeader, CardTitle } from "./ui/card";
 import { ChartContainer } from "./ui/chart";
 
 type ScoreCountHistogramDatum = {
@@ -52,6 +53,19 @@ const BasicStatsDisplay: React.FC = () => {
   // When the gridStats.scoreCounts changes, update the scoreCountsList
   useEffect(() => {
     if (gridStats.scoreCounts) {
+      // For debug
+      // setScoreCountsList([
+      //   { score: 0, count: 1 },
+      //   { score: 1, count: 2 },
+      //   { score: 2, count: 30 },
+      //   { score: 3, count: 15 },
+      //   { score: 4, count: 5 },
+      //   { score: 5, count: 6 },
+      //   { score: 6, count: 18 },
+      //   { score: 7, count: 21 },
+      //   { score: 8, count: 7 },
+      //   { score: 9, count: 10 },
+      // ]);
       setScoreCountsList(convertScoreCountsToBarChartData(gridStats.scoreCounts));
     }
   }, [gridStats.scoreCounts]);
@@ -66,6 +80,11 @@ const BasicStatsDisplay: React.FC = () => {
       type: "number",
     },
   };
+
+  // Set the Y axis domain to be 10% higher than the highest count
+  // so that the highest bar doesn't touch the top of the chart
+  // and there's room for the count labels
+  const MAX_Y_AXIS_VALUE = Math.max(...scoreCountsList.map((datum) => datum.count)) * 1.1;
 
   const content = (
     <div className="w-full h-full flex flex-col items-center">
@@ -86,14 +105,41 @@ const BasicStatsDisplay: React.FC = () => {
           </div>
         ))}
 
+        <br></br>
+
         {/* Score bar chart */}
         {scoreCountsList && (
-          <ChartContainer config={SCORE_COUNTS_BAR_CHART_CONFIG}>
-            <BarChart data={scoreCountsList}>
-              <XAxis dataKey="score" tickLine={false} axisLine={false} />
-              <Bar dataKey={"count"} fill="#8884d8" radius={12} />
-            </BarChart>
-          </ChartContainer>
+          <Card className="bg-theme-primary border-theme-light-secondary dark:border-theme-dark-secondary">
+            <CardHeader>
+              <CardTitle className="mx-auto theme-text">Today's scores</CardTitle>
+            </CardHeader>
+            <ChartContainer config={SCORE_COUNTS_BAR_CHART_CONFIG}>
+              <BarChart data={scoreCountsList}>
+                <XAxis dataKey="score" tickLine={false} axisLine={false} />
+                <YAxis
+                  dataKey="count"
+                  domain={[0, MAX_Y_AXIS_VALUE]}
+                  tickLine={false}
+                  axisLine={false}
+                  hide={true}
+                />
+                {/* TODO: Make bar width and font size responsive */}
+                <Bar
+                  dataKey={"count"}
+                  className="fill-theme-light-secondary dark:fill-theme-dark-secondary"
+                  radius={12}
+                  minPointSize={10}
+                  barSize={10}
+                >
+                  <LabelList
+                    position="top"
+                    className="fill-theme-light-text dark:fill-theme-dark-text"
+                    style={{ fontSize: "20px" }}
+                  />
+                </Bar>
+              </BarChart>
+            </ChartContainer>
+          </Card>
         )}
       </div>
     </div>
