6b6ed9e8c42f5ca544df94ef5fbe9b5fb02d1d9e
Josh Clark <jclarktennis@gmail.com>
2025-03-12 15:11:53 -0400

    feat: add generic algorithm for 'scrapeData'

--- grid_generation/src/adapters/data_scrapers/movies/movieDataScraper.ts
+++ grid_generation/src/adapters/data_scrapers/movies/movieDataScraper.ts
@@ -1,6 +1,71 @@
-import { MovieGraphDataWithGenres } from "src/adapters/graph/movies";
+import {
+  ActorOrCategoryData,
+  CreditData,
+  MovieGraphData,
+  MovieGraphDataWithGenres,
+} from "src/adapters/graph/movies";
 import DataScraper from "src/ports/dataScraper";
+import { AxisEntityData, ConnectionData, LinkData } from "src/ports/graph";
 
 export default abstract class MovieDataScraper extends DataScraper {
-  abstract scrapeData(): Promise<MovieGraphDataWithGenres>;
+  /**
+   * Scrape actor/credit data from a data source and return the graph data to populate the data store with.
+   *
+   * We also get genre information, since it's needed to populate the data store.
+   *
+   * @param existingAxisEntities the actors that already exist in the data store
+   * @returns the graph data to populate the data store with
+   */
+  async scrapeData(existingAxisEntities: {
+    [key: string]: ActorOrCategoryData;
+  }): Promise<MovieGraphDataWithGenres> {
+    const graphData = (await super.scrapeData(existingAxisEntities)) as MovieGraphData;
+    const genres = await this.getGenres();
+
+    return {
+      ...graphData,
+      genres: genres,
+    };
+  }
+
+  /**
+   * Get new actors from the data source.
+   *
+   * This is an alias for getNewAxisEntities that specifically gets new actors.
+   *
+   * @returns a promise that resolves to a dictionary of new actors
+   */
+  abstract getNewActors(): Promise<{ [key: string]: ActorOrCategoryData }>;
+
+  /**
+   * Get credit information for a list of actors.
+   *
+   * This is an alias for getConnectionsForAxisEntitiesWithLinks that
+   * specifically gets credit information for actors.
+   *
+   * @param actors the actors to get credit information for
+   * @returns a promise that resolves to a dictionary of connections and a list of links
+   */
+  abstract getCreditsForActorsWithLinks(actors: { [key: string]: ActorOrCategoryData }): Promise<{
+    connections: { [key: string]: CreditData };
+    links: LinkData[];
+  }>;
+
+  /**
+   * Get genre information from the data source.
+   *
+   * @returns a promise that resolves to a dictionary of genre IDs and names
+   */
+  abstract getGenres(): Promise<{ [key: number]: string }>;
+
+  async getNewAxisEntities(): Promise<{ [key: string]: AxisEntityData }> {
+    return await this.getNewActors();
+  }
+
+  async getConnectionsForAxisEntitiesWithLinks(axisEntities: { [key: string]: AxisEntityData }): Promise<{
+    connections: { [key: string]: ConnectionData };
+    links: LinkData[];
+  }> {
+    return await this.getCreditsForActorsWithLinks(axisEntities);
+  }
 }
--- grid_generation/src/ports/dataScraper.ts
+++ grid_generation/src/ports/dataScraper.ts
@@ -1,5 +1,66 @@
-import { GraphData } from "./graph";
+import { AxisEntityData, ConnectionData, GraphData, LinkData } from "./graph";
 
 export default abstract class DataScraper {
-  abstract scrapeData(): Promise<GraphData>;
+  /**
+   * Scrape data from a data source and return the graph data to populate the data store with.
+   *
+   * We pass in a dictionary of existing axis entities so that their connections are updated along
+   * with any new axis entities' connections.
+   *
+   * @param existingAxisEntities the axis entities that already exist in the data store
+   * @returns the graph data to populate the data store with
+   */
+  async scrapeData(existingAxisEntities: { [key: string]: AxisEntityData }): Promise<GraphData> {
+    /* 1. Get all axis entities */
+    //  a. Get new axis entities
+    const newAxisEntities = await this.getNewAxisEntities();
+    //  b. Combine new and existing axis entities
+    const allAxisEntities = this.mergeAndDeduplicateAxisEntities(newAxisEntities, existingAxisEntities);
+
+    /* 2. Get all connection and link information for axis entities */
+    const { connections, links } = await this.getConnectionsForAxisEntitiesWithLinks(allAxisEntities);
+
+    return {
+      axisEntities: allAxisEntities,
+      connections: connections,
+      links: links,
+    };
+  }
+
+  /**
+   * Get new axis entities from the data source.
+   *
+   * It's up to the implementation to determine how new axis entities are determined.
+   *
+   * @returns a promise that resolves to a dictionary of new axis entities
+   */
+  abstract getNewAxisEntities(): Promise<{ [key: string]: AxisEntityData }>;
+
+  /**
+   * Get connection information for a list of axis entities.
+   *
+   * @param axisEntities the axis entities to get connection information for
+   * @returns a promise that resolves to a dictionary of connections and a list of links
+   */
+  abstract getConnectionsForAxisEntitiesWithLinks(axisEntities: { [key: string]: AxisEntityData }): Promise<{
+    connections: { [key: string]: ConnectionData };
+    links: LinkData[];
+  }>;
+
+  /**
+   * Merge new axis entities with existing axis entities and remove duplicates.
+   *
+   * @param newAxisEntities the new axis entities
+   * @param existingAxisEntities the existing axis entities
+   * @returns a dictionary of axis entities with duplicates removed
+   */
+  mergeAndDeduplicateAxisEntities(
+    newAxisEntities: { [key: string]: AxisEntityData },
+    existingAxisEntities: { [key: string]: AxisEntityData }
+  ): { [key: string]: AxisEntityData } {
+    return {
+      ...newAxisEntities,
+      ...existingAxisEntities,
+    };
+  }
 }
